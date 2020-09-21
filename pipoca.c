74e58629a029fe28207944c9545871b304bdffae
Luigino De Togni <lewix@MacBook-Pro-di-Luigino.local>
2020-09-21 17:52:21 +0200

    check limits added

--- pipoca.c
+++ pipoca.c
@@ -2,24 +2,46 @@
 #include <stdio.h>
 #include <time.h>
 
+#define MAX_ROWS 100000000
+#define MAX_STRING_LENGTH 100
+
+void printRandomStrings(unsigned int nRows, unsigned int nChars){
+    
+    unsigned int rowNumber, colNumber;
+
+    srand(time(NULL));
+
+    for (rowNumber = 0; rowNumber < nRows; rowNumber++)
+    {
+        for (colNumber = 0; colNumber < nChars; colNumber++)
+        {
+            //unsigned int currentSeed = time();
+            printf("%i",rand()%10);
+        }
+        printf("\n");
+    }
+}
+
 int main(int len, const char* args[])
 {
+    int prmNRows, prmLString;
+
     // not enough parameters
     if (len < 3)
     {
         printf("pipoca [random string length] [number of rows to generate]\n");
-        return 0;
+        return 1;
     }
 
-    unsigned int seedInt = time(NULL)%1000;
-    unsigned int rowNumber, colNumber;
-    for (rowNumber = 0; rowNumber < atoi(args[2]); rowNumber++)
+    // check limits
+    prmNRows = atoi(args[2]);
+    prmLString = atoi(args[1]);
+
+    if (prmNRows <= 0 || prmNRows > MAX_ROWS || prmLString <= 0 || prmLString > MAX_STRING_LENGTH)
     {
-        for (colNumber = 0; colNumber < atoi(args[1]); colNumber++)
-        {
-            //unsigned int currentSeed = time();
-            printf("%i",rand_r(&seedInt)%10);
-        }
-        printf("\n");
+        printf("pipoca [random string 1-%i] [number of rows to generate 1-%i]\n",MAX_STRING_LENGTH,MAX_ROWS);
+        return 1;
     }
+
+    printRandomStrings(prmNRows,prmLString);
 }
